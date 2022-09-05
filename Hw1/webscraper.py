0ec7ffa2f097a30e4df807181da7693f3c275361
Asa Guest <acg70@duke.edu>
2022-09-05 18:06:08 -0400

    Original Commit

--- Hw1/webscraper.py
+++ Hw1/webscraper.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import wbgapi as wb
+
 
 
 def OpenBrowser(driver, webaddress):
@@ -21,19 +23,22 @@ def country_picker(driver, countries):
         driver.find_element(By.XPATH,"[WDI_Ctry].[List].&[PAK]").click()
 
 
-# Set path Selenium
-CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
-s = Service(CHROMEDRIVER_PATH)
-WINDOW_SIZE = "1920,1080"
 
-#Create driver
-driver = webdriver.Chrome(service=s)
-world_data_url = "https://databank.worldbank.org/source/world-development-indicators"
-OpenBrowser(driver, world_data_url)
-time.sleep(3)
-driver.find_element(By.ID,"chk[WDI_Ctry].[List].&[ARG]").click()
-driver.find_element(By.XPATH, '//a[@href="#selectedDimension_WDI_Series"]').click()
-time.sleep(3)
-driver.find_element(By.ID,"chk[WDI_Series].[Topic].&[EG.CFT.ACCS.ZS]").click()
+#https://pypi.org/project/wbgapi/
+
+# # Set path Selenium
+# CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+# s = Service(CHROMEDRIVER_PATH)
+# WINDOW_SIZE = "1920,1080"
+#
+# #Create driver
+# driver = webdriver.Chrome(service=s)
+# world_data_url = "https://databank.worldbank.org/source/world-development-indicators"
+# OpenBrowser(driver, world_data_url)
+# time.sleep(3)
+# driver.find_element(By.ID,"chk[WDI_Ctry].[List].&[ARG]").click()
+# driver.find_element(By.XPATH, '//a[@href="#selectedDimension_WDI_Series"]').click()
+# time.sleep(3)
+# driver.find_element(By.ID,"chk[WDI_Series].[Topic].&[EG.CFT.ACCS.ZS]").click()
 
 
